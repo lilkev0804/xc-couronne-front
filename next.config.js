bd5887fdeed287c0272b99b228272948b84ac092
kevin.lemarie <kevin.l@now.online>
2023-04-06 19:30:28 +0200

    add main package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  optimizeFonts: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  experimental: {
+    outputStandalone: true,
+  },
+  experimental: {
+    externalDir: true,
+  },
+};
+
+module.exports = nextConfig;
